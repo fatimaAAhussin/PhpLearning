1783d680c5b90c4c0bd3834ec81b2d25abad164a
fatimaAAhussin <fm053356@gmail.com>
2023-10-30 23:14:31 +0300

    first commit

---
+++ for.php
@@ -0,0 +1,11 @@
+<?php
+$family =['Fatimah','Anas','Majed'];
+
+foreach($family as $member){
+    echo $member .'<br>';
+}
+// for ($i=0; $i <count($family) ; $i++) { 
+//     echo $family[$i].'<br>';
+// }
+
+?>
